3c04210ffec6074ced7288049ac6da5f9807d427
likachedia <likachedia@gmail.com>
2022-03-15 19:26:02 +0400

    init: add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+export interface Task {
+  title: string,
+  difficulty: string,
+  state?: string,
+}
+
+export enum State {
+  todo = 'To-Do',
+  inProgres = 'In Progres',
+  done = 'Done'
+}
+
+export enum Difficulty {
+  easy = 'easy',
+  medium = 'medium',
+  hard = 'hard',
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +24,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  @Input()
+  // empty: TemplateRef | undefined;
+  title:string = 'todo-app';
+  card_titels: string[] = ['To-Do', 'In Porgres', 'Done'];
+  difficulties = [Difficulty.easy, Difficulty.medium, Difficulty.hard];
+  todo_list: Task[] = [
+    {
+      title: 'do homework',
+      difficulty: 'medium',
+      state: 'todo',
+    }
+  ];
+  inProgres_list: Task[] = [];
+  done_list: Task[] = [];
+  list_items: Task[] = [
+  ];
+  task: Task = {
+    title: "",
+    difficulty: "",
+    state: State.done,
+  }
+  selectedOption: string = "";
+  task_title: string = "";
+
+  addTask(title: string, difficulty: string) {
+    let task: Task = {
+      title: title,
+      difficulty: difficulty,
+      state: State.done,
+    }
+    this.todo_list.push(task);
+  }
+
+  moveItem(i: number) {
+    this.inProgres_list.push(this.todo_list[i])
+    this.todo_list.splice(i, 1);
+  }
 }
